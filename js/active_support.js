c9bf10c9d51385f340dd66698a766ba668b3a3eb
Andy <andynu@gmail.com>
2024-02-23 22:04:28 -0500

    Remove pluralizeMethods
    So this is easier to just do as a loop locally within each module. No
    need for this.

--- js/active_support.js
+++ js/active_support.js
@@ -1,12 +1,6 @@
 class ActiveSupport {
   static Version = '0.1';
 
-  static pluralizeMethods(module) {
-    Object.entries(module).forEach(([key, value]) => {
-      module[`${key}s`] = value;
-    });
-  }
-
   static pluralize(count, singular) {
     return Math.abs(count) === 1 ? `${count} ${singular}` : `${count} ${singular}s`;
   }
